5f9aadfe017195fb1402ed7281be9d1136054fc9
Jimmaphy <wschaijk@outlook.com>
2025-11-21 14:48:40 +0100

    Moved Models

--- broker/infrastructure/postgresql.go
+++ broker/infrastructure/postgresql.go
@@ -1,6 +1,7 @@
 package infrastructure
 
 import (
+	"database/sql"
 	"hotelhub/broker/services"
 )
 
@@ -14,4 +15,22 @@ func NewPostgres(configuration *services.Configuration) *Postgres {
 	return &Postgres{
 		connectionString: configuration.DatabasePath,
 	}
+}
+
+// ExecuteQuery executes a given SQL query on the PostgreSQL database.
+// The result of the query is ignored.
+// Errors occuring during the execution are directly returned.
+func (postgres *Postgres) ExecuteQuery(query string) error {
+	connection, err := sql.Open("postgres", postgres.connectionString)
+	if err != nil {
+		return err
+	}
+
+	defer connection.Close()
+	_, err = connection.Exec(query)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
